00d3a9f674952e349ed739dfd8683aa59d6d1156
StrtMilne <oober@hotmail.co.uk>
2021-10-29 13:10:15 +0100

    finished MVP

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,9 +3,9 @@ from app import app
 from models.player import Player
 from models.game import Game
 
-@app.route("/<one_choice>/<two_choice")
+@app.route("/<one_choice>/<two_choice>")
 def index(one_choice, two_choice):
-    player_1 = Player("player_1", one_choice)
-    player_2 = Player("player_2", two_choice)
-    winner = Game(player_1, player_2)
-    return render_template("index.html", title="Home", one_choice=one_choice, two_choice=two_choice, winner=winner)
+    player_1 = Player("Player 1", one_choice)
+    player_2 = Player("Player 2", two_choice)
+    winner_object = Game.winner_is(player_1, player_2)
+    return render_template("played.html", title="Home", one_choice=one_choice, two_choice=two_choice, winner=winner_object)
