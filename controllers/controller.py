54cc1e271e5e88934e2412793b5a76ed620863dc
StrtMilne <oober@hotmail.co.uk>
2021-10-29 14:36:18 +0100

    added style to basic MVP set-up

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,9 +3,13 @@ from app import app
 from models.player import Player
 from models.game import Game
 
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home")
+
 @app.route("/<one_choice>/<two_choice>")
-def index(one_choice, two_choice):
+def played(one_choice, two_choice):
     player_1 = Player("Player 1", one_choice)
     player_2 = Player("Player 2", two_choice)
     winner_object = Game.winner_is(player_1, player_2)
-    return render_template("played.html", title="Home", one_choice=one_choice, two_choice=two_choice, winner=winner_object)
+    return render_template("played.html", title="You played a game!", one_choice=one_choice, two_choice=two_choice, winner=winner_object)
