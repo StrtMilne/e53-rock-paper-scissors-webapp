dd70fad16264167819186fd32e1b88555116b4f8
StrtMilne <oober@hotmail.co.uk>
2021-10-29 18:08:45 +0100

    made further edits, just can't get post form to work

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,11 +3,11 @@ from app import app
 from models.player import Player
 from models.game import Game
 
-@app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html", title="Home")
 
-@app.route("/<one_choice>/<two_choice>")
+@app.route("/home/<one_choice>/<two_choice>")
 def played(one_choice, two_choice):
     player_1 = Player("Player 1", one_choice)
     player_2 = Player("Player 2", two_choice)
@@ -18,8 +18,11 @@ def played(one_choice, two_choice):
 def computer_game():
     return render_template("play.html", title="Play the computer!")
     
-# @app.route("/play/<choice>", methods=["post"])
-# def computer_game(choice):
-#     winner_object = Game.play_computer(choice)
-#     winner = winner_object.name
-#     return render_template("play.html", title="Play the computer!")
+@app.route("/play?choice=<chosen>", methods=["POST"])
+def play_computer(chosen):
+    pick = chosen
+    player_1 = Player("Player 1", pick)
+    player_2 = Game.create_player()
+    player_2_choice = player_2.choice
+    winner_object = Game.winner_is(player_1, player_2)
+    return render_template("played.html", title="You played the computer!", one_choice=pick, two_choice=player_2_choice, winner=winner_object)
